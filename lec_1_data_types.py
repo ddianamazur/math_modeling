259e706e023fed0714f4c2106a50136080a68434
ddianamazur <144795343+ddianamazur@users.noreply.github.com>
2023-09-19 14:35:23 +0200

    раврс

---
+++ lec_1_data_types.py
@@ -0,0 +1,23 @@
+type 
+
+type(3)
+
+print(type(3))
+
+print(type(3.4))
+
+print(type('молодец'))
+
+a = 'molodec'
+
+print(type(a))
+
+a = ['molodec', 1, 'solo', 6, 8]
+
+print(type(a))
+
+print(type(True))
+
+print(type(False))
+
+print(type(None))
